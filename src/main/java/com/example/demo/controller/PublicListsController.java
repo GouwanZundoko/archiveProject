6255cf214056f18d9abb83074b937cf871120358
GouwanZundoko <uraakaut@gmail.com>
2026-04-14 21:30:49 +0900

    リスト表示部分の追加

--- src/main/java/com/example/demo/controller/PublicListsController.java
+++ src/main/java/com/example/demo/controller/PublicListsController.java
@@ -7,9 +7,17 @@ import org.springframework.web.bind.annotation.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import jakarta.servlet.http.HttpSession;
+
+import com.example.demo.sql.SelectMyContents;
+import com.example.demo.sql.SelectMyLists;
 import com.example.demo.sql.SelectPublicLists;
+import com.example.demo.dto.MyContentsDto;
+import com.example.demo.dto.MyContentsListDto;
 import com.example.demo.dto.MyListsDto;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class PublicListsController {
@@ -17,6 +25,12 @@ public class PublicListsController {
     private final PublicListRepository repository;
     @Autowired
     SelectPublicLists selectPublicLists;
+
+    @Autowired
+    SelectMyLists selectMyLists;
+
+    @Autowired
+    private SelectMyContents selectMyContents;
 
     // ★ これを追加（重要）
     public PublicListsController(PublicListRepository repository) {
@@ -27,12 +41,85 @@ public class PublicListsController {
     public String showLists(
             @RequestParam(required = false) String keyword, HttpSession session, Model model) {
 
+        String UserId = (String) session.getAttribute("UserId");
         String CompanyId = (String) session.getAttribute("CompanyId");
 
-        List<MyListsDto> lists = selectPublicLists.GetAllPublicLists(CompanyId, "1");
-
+        List<MyListsDto> lists = selectPublicLists.GetAllPublicLists(UserId, CompanyId, "1");
         model.addAttribute("lists", lists);
 
+        // ★モーダル用コンテンツ
+        List<MyContentsDto> contentsList = selectMyContents.GetAllMyContents(UserId, CompanyId);
+        model.addAttribute("contentsList", contentsList);
+
         return "public-lists";
+    }
+
+    @GetMapping("/public-lists/selected-contents/{listId}")
+    @ResponseBody
+    public List<MyContentsListDto> getSelectedContents(@PathVariable String listId) {
+        return selectMyLists.getContentsByListId(listId);
+    }
+
+    @PostMapping("/public-lists/add-contents")
+    @ResponseBody
+    public String addContents(@RequestBody Map<String, Object> body, HttpSession session) {
+
+        String userId = (String) session.getAttribute("UserId");
+        String companyId = (String) session.getAttribute("CompanyId");
+        String listId = (String) body.get("listId");
+        List<?> rawList = (List<?>) body.get("contentsIds");
+
+        List<String> contentsIds = new ArrayList<>();
+        for (Object obj : rawList) {
+            contentsIds.add(String.valueOf(obj));
+        }
+
+        // 既存削除処理
+        selectMyLists.DeleteMyContentsList(userId, companyId, listId);
+
+        // 追加処理
+        selectMyLists.InsertPublicList(listId, userId, companyId, contentsIds);
+
+        return "redirect:/public-lists";
+    }
+
+    @GetMapping("/public-lists/release/{id}/{release}")
+    public String contentsRelease(HttpSession session, Model model, @PathVariable("id") String productId,
+            @PathVariable("release") String releaseFlg) {
+        // DB取得は自分で実装
+        String UserId = (String) session.getAttribute("UserId");
+        String CompanyId = (String) session.getAttribute("CompanyId");
+        int selectCount = selectMyLists.UpdateMyLists(UserId, CompanyId, productId, releaseFlg);
+        return "redirect:/public-lists";
+    }
+
+    @GetMapping("/public-lists/delete/{id}")
+    public String listsDelete(HttpSession session, Model model, @PathVariable("id") String productId) {
+        // DB取得は自分で実装
+        String UserId = (String) session.getAttribute("UserId");
+        String CompanyId = (String) session.getAttribute("CompanyId");
+        // DTO使用
+        List<MyListsDto> lists = selectMyLists.GetOneMyLists(UserId, CompanyId, productId);
+        MyListsDto dto = lists.get(0);
+        String Auth = dto.getShowAuth();
+        // マイコンテンツ側を削除
+        int deleteMyCount = selectMyLists.DeleteMyLists(UserId, CompanyId, productId);
+
+        if ("1".equals(Auth)) {
+            // 自分で作成したコンテンツの場合コンテンツも削除
+            int deletePublicCount = selectMyLists.DeletePublicLists(UserId, CompanyId, productId);
+            deletePublicCount = selectMyLists.DeletePublicContentsList(UserId, CompanyId, productId);
+        }
+
+        return "redirect:/public-lists";
+    }
+
+    @GetMapping("/public-lists/import/{id}")
+    public String contentsImport(HttpSession session, Model model, @PathVariable("id") String productId) {
+        // DB取得は自分で実装
+        String UserId = (String) session.getAttribute("UserId");
+        String CompanyId = (String) session.getAttribute("CompanyId");
+        selectPublicLists.ImportPublicLists(UserId, CompanyId, productId);
+        return "redirect:/public-lists";
     }
 }
--- src/main/java/com/example/demo/sql/SelectPublicLists.java
+++ src/main/java/com/example/demo/sql/SelectPublicLists.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 import com.example.demo.dto.MyListsDto;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class SelectPublicLists {
@@ -15,29 +16,38 @@ public class SelectPublicLists {
     @Autowired
     EntityManager entityManager;
 
-    public List<MyListsDto> GetAllPublicLists(String CompanyId, String ShowAuth) {
+    public List<MyListsDto> GetAllPublicLists(String UserId, String CompanyId, String ShowAuth) {
 
         StringBuilder sb = new StringBuilder();
         sb.append(" SELECT ");
         sb.append("     c002.lists_id ");
-        sb.append("     , c002.company_id ");
-        sb.append("     , c002.user_id ");
-        sb.append("     , c002.lists_title ");
-        sb.append("     , c002.lists_text ");
-        sb.append("     , c002.show_auth  ");
+        sb.append("   , c002.company_id ");
+        sb.append("   , c002.user_id ");
+        sb.append("   , c002.lists_title ");
+        sb.append("   , c002.lists_text ");
+        sb.append("   , c002.show_auth ");
+        sb.append("   , c002.created_at ");
+        sb.append("   , c002.updated_at ");
+        sb.append("   , m002.show_auth ");
         sb.append(" FROM ");
-        sb.append("     c002_lists_info c002  ");
+        sb.append("     c002_lists_info c002 ");
+        sb.append(" LEFT JOIN ");
+        sb.append("     m002_mylists_info m002 ");
+        sb.append(" ON ");
+        sb.append("     m002.lists_id = c002.lists_id ");
+        sb.append("     AND m002.user_id = :user_id ");
+        sb.append("     AND m002.company_id = :company_id ");
         sb.append(" WHERE ");
-        sb.append("     c002.company_id = :company_id  ");
+        sb.append("     c002.company_id = :company_id ");
         sb.append("     AND c002.show_auth = :show_auth ");
 
         Query query = entityManager.createNativeQuery(sb.toString());
+        query.setParameter("user_id", UserId);
         query.setParameter("company_id", CompanyId);
         query.setParameter("show_auth", ShowAuth);
 
         List<Object[]> result = query.getResultList();
 
-        // DTO変換
         List<MyListsDto> dtoList = new ArrayList<>();
 
         for (Object[] row : result) {
@@ -48,11 +58,37 @@ public class SelectPublicLists {
             dto.setUserId(String.valueOf(row[2]));
             dto.setListsTitle((String) row[3]);
             dto.setListsText((String) row[4]);
-            dto.setShowAuth(String.valueOf(row[5]));
+            dto.setShowPublicAuth(String.valueOf(row[5]));
+            dto.setCreatedAt(String.valueOf(row[6]));
+            dto.setUpdatedAt(String.valueOf(row[7]));
+            dto.setShowAuth(String.valueOf(row[8]));
 
             dtoList.add(dto);
         }
 
         return dtoList;
+    }
+
+    @Transactional
+    public void ImportPublicLists(String userId, String companyId, String contentsId) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(" INSERT INTO m002_mylists_info ( ");
+        sb.append("     company_id, ");
+        sb.append("     user_id, ");
+        sb.append("     lists_id, ");
+        sb.append("     show_auth ");
+        sb.append(" ) VALUES ( ");
+        sb.append("     :companyId, ");
+        sb.append("     :userId, ");
+        sb.append("     :lists_id, ");
+        sb.append("     '0' ");
+        sb.append(" ) ");
+
+        Query query = entityManager.createNativeQuery(sb.toString());
+        query.setParameter("userId", userId);
+        query.setParameter("companyId", companyId);
+        query.setParameter("lists_id", contentsId);
+
+        query.executeUpdate();
     }
 }
